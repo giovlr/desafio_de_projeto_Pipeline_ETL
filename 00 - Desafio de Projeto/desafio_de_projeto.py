fe01c27a780eeeced0ee40ed1165abe11179367a
giovlr <vlrgiovana@gmail.com>
2023-10-19 17:52:39 -0300

    commit_codigo_python

---
+++ 00 - Desafio de Projeto/desafio_de_projeto.py
@@ -0,0 +1,28 @@
+import openai
+
+openai.api_key = openai_api_key
+
+def generate_ai_news(user):
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {
+          "role": "system",
+          "content": "Você é um especialista em markting bancário."
+      },
+      {
+          "role": "user",
+          "content": f"Crie uma mensagem personalizada de boas-vindas para {user['name']} falando um benefício de ter conta no banco Santander (máximo de 100 caracteres)"
+      }
+    ]
+  )
+  return completion.choices[0].message.content.strip('\"')
+
+for user in users:
+  news = generate_ai_news(user)
+  print(news)
+
+  user['news'].append({
+      "icon": "https://digitalinnovationone.github.io/santander-dev-week-2023-api/icons/credit.svg",
+      "description": news
+  })
